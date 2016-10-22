622ffea44baa76185d9daf97049d90cca435793d
Richard Hannah <richard.hannah1974@gmail.com>
2016-10-22 16:32:08 +0100

    added simple prime factor algorithm

--- src/main/java/com/richard/PrimeFactorGenerator.java
+++ src/main/java/com/richard/PrimeFactorGenerator.java
@@ -1,10 +1,29 @@
 package com.richard;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.math3.primes.Primes;
+
+import java.util.List;
+
 /**
  * Created by highl on 22/10/2016.
  */
 public class PrimeFactorGenerator implements FactorGenerator {
     public long[] generateFactors(int numberOfFactors) {
-        return new long[numberOfFactors];
+        return simplePrimeFactors(numberOfFactors);
+    }
+
+    private long[] simplePrimeFactors(int numberOfFactors){
+
+        long[] factors = new long[numberOfFactors];
+
+        int position = 0;
+        for(int i = 0; i < factors.length; i++){
+            if(Primes.isPrime(i)){
+                factors[position] = i;
+                position++;
+            }
+        }
+        return factors;
     }
 }
