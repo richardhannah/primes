1f88e69cac67785e7bd3124e2566cae2cff0ab57
Richard Hannah <richard.hannah1974@gmail.com>
2016-10-22 14:28:32 +0100

    added Multiplicationtablebuilder

---
+++ src/main/java/com/richard/MultiplicationTableBuilder.java
@@ -0,0 +1,13 @@
+package com.richard;
+
+/**
+ * Created by highl on 22/10/2016.
+ */
+public interface MultiplicationTableBuilder {
+
+    void setTable(long[][] table);
+    void setFormatter(TableFormatter tableFormatter);
+
+
+
+}
