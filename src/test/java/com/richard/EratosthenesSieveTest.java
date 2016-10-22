cae3de5b4bafc893719ec99b606a327bf226c755
Richard Hannah <richard.hannah1974@gmail.com>
2016-10-22 23:18:21 +0100

    set up Eratosthenes test

--- src/test/java/com/richard/EratosthenesSieveTest.java
+++ src/test/java/com/richard/EratosthenesSieveTest.java
@@ -1,5 +1,7 @@
 package com.richard;
 
+import org.apache.commons.math3.primes.Primes;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,9 +10,25 @@ import static org.junit.Assert.*;
  * Created by highl on 22/10/2016.
  */
 public class EratosthenesSieveTest {
+
+    PrimesAlgorithm primesAlgorithm;
+
+    @Before
+    public void setup(){
+       primesAlgorithm = new EratosthenesSieve();
+    }
+
+
     @Test
     public void getPrimes() throws Exception {
+        int numfactors =3;
 
+        long[] primes = primesAlgorithm.getPrimes(numfactors);
+
+        assertTrue(primes.length == numfactors);
+        for(long prime : primes){
+            assertTrue(Primes.isPrime((int)prime));
+        }
     }
 
 }
