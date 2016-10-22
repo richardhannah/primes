398aa599d87f4ae364b3ee8809d8f768dc51c788
Richard Hannah <richard.hannah1974@gmail.com>
2016-10-22 23:10:43 +0100

    added Sieve of Eratosthenes algorithm

--- src/main/java/com/richard/EratosthenesSieve.java
+++ src/main/java/com/richard/EratosthenesSieve.java
@@ -3,5 +3,30 @@ package com.richard;
 /**
  * Created by highl on 22/10/2016.
  */
-public class EratosthenesSieve {
+public class EratosthenesSieve implements PrimesAlgorithm {
+    @Override
+    public long[] getPrimes(int n) {
+
+        long[] primes = new long[n];
+        //primes[0] = 2;
+
+        boolean[] primesBool = new boolean[n +1];
+        for(boolean primebool : primesBool){
+            primebool = true;
+        }
+        primesBool[0] = false;
+        primesBool[1] = false;
+        int primesIndex = 0;
+        for (int i = (int)primes[primesIndex]; i < n; i++ ){
+           if(primesBool[i]){
+               for(int j =(int)primes[i]; i*j <= n; j++ ){
+                   primesBool[i*j]=false;
+               }
+           }
+            primes[primesIndex + 1] = i;
+        }
+
+        return primes;
+    }
+
 }
---
+++ src/test/java/com/richard/EratosthenesSieveTest.java
@@ -0,0 +1,16 @@
+package com.richard;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by highl on 22/10/2016.
+ */
+public class EratosthenesSieveTest {
+    @Test
+    public void getPrimes() throws Exception {
+
+    }
+
+}
