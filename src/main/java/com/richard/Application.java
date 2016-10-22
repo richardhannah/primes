b2733437fe8c7dffa369815354cabc7db773a0df
Richard Hannah <richard.hannah1974@gmail.com>
2016-10-22 19:23:21 +0100

    first draft of full program - not quite working properly yet

--- src/main/java/com/richard/Application.java
+++ src/main/java/com/richard/Application.java
@@ -7,7 +7,23 @@ public class Application {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, World");
+
+
+        System.out.println("Prime number multiplication table");
+        int dimensions = 1;
+        try {
+            dimensions = System.in.read();
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        MultiplicationTableBuilder tableBuilder = new PrimesTableBuilder()
+                .withFormatter(new PrimesTableFormatter())
+                .withFactorGenerator(new PrimeFactorGenerator());
+        FormattedTable table = tableBuilder.build(dimensions);
+        table.display();
+
     }
 
 
