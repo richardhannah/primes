3f3a90d81cb9fcadb856631ca144674a5f5509e2
Richard Hannah <richard.hannah1974@gmail.com>
2016-10-22 11:04:37 +0100

    added main application class

---
+++ src/main/java/com/richard/Application.java
@@ -0,0 +1,15 @@
+package com.richard;
+
+/**
+ * Created by highl on 22/10/2016.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World");
+    }
+
+
+
+}
