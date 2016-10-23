e2f3371588357165f2b319d65613e8f5732992c4
Richard Hannah <richard.hannah1974@gmail.com>
2016-10-23 19:02:35 +0100

    added input validation

--- src/main/java/com/richard/Application.java
+++ src/main/java/com/richard/Application.java
@@ -10,7 +10,17 @@ public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Prime number multiplication table");
-        int dimensions = Integer.valueOf(scanner.nextLine());
+        int dimensions;
+
+        do {
+            System.out.println("Enter size of grid");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Invalid entry");
+                scanner.next();
+            }
+            dimensions = scanner.nextInt();
+        } while (dimensions <= 0);
+
 
         MultiplicationTableBuilder tableBuilder = new PrimesTableBuilder()
                 .withFormatter(new PrimesTableFormatter())
@@ -19,7 +29,4 @@ public class Application {
         table.display();
 
     }
-
-
-
 }
