b4f60c1cf851654fcf32e733d1a0604a8d904eef
Richard Hannah <richard.hannah1974@gmail.com>
2016-10-23 21:37:35 +0100

    added algorithm selection to UI

--- src/main/java/com/richard/Application.java
+++ src/main/java/com/richard/Application.java
@@ -10,8 +10,49 @@ public class Application {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Prime number multiplication table");
-        int dimensions;
+        PrimesAlgorithm algorithm = selectAlgorithm(scanner);
+        int dimensions = collectInput(scanner);
 
+        MultiplicationTableBuilder tableBuilder = new PrimesTableBuilder()
+                .withFormatter(new PrimesTableFormatter())
+                .withAlgorithm(algorithm);
+        FormattedTable table = tableBuilder.build(dimensions);
+        table.display();
+        main(new String[]{""});
+
+    }
+
+    private static PrimesAlgorithm selectAlgorithm(Scanner scanner){
+        int choice;
+        System.out.println("Select Algorithm");
+        System.out.println("1. Apache commons math3 library");
+        System.out.println("2. Sieve of Eratosthenes");
+        System.out.println("3. Exit");
+        do {
+            System.out.println("Enter selection");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Invalid entry");
+                scanner.next();
+            }
+            choice = scanner.nextInt();
+        } while (choice <= 0);
+
+        switch (choice){
+            case 1:
+                return new SimplePrimes();
+            case 2:
+                return new EratosthenesSieve();
+            case 3:
+                System.exit(0);
+            default:
+                System.out.println("Invalid selection");
+                selectAlgorithm(scanner);
+        }
+        return null;
+    }
+
+    private static int collectInput(Scanner scanner){
+        int dimensions;
         do {
             System.out.println("Enter size of grid");
             while (!scanner.hasNextInt()) {
@@ -20,13 +61,6 @@ public class Application {
             }
             dimensions = scanner.nextInt();
         } while (dimensions <= 0);
-
-
-        MultiplicationTableBuilder tableBuilder = new PrimesTableBuilder()
-                .withFormatter(new PrimesTableFormatter())
-                .withAlgorithm(new EratosthenesSieve());
-        FormattedTable table = tableBuilder.build(dimensions);
-        table.display();
-
+        return dimensions;
     }
 }
