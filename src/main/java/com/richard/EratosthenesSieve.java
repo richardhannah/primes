68d628dafbd8445fe6781cfa361278d81d40b010
Richard Hannah <richard.hannah1974@gmail.com>
2016-10-23 22:08:31 +0100

    optimize Eratosthenes sieve - now about 4x faster

--- src/main/java/com/richard/EratosthenesSieve.java
+++ src/main/java/com/richard/EratosthenesSieve.java
@@ -5,7 +5,12 @@ package com.richard;
  */
 public class EratosthenesSieve implements PrimesAlgorithm {
 
-    int range = 10000;
+    int range = 8;
+    boolean[] primes;
+
+    public EratosthenesSieve(){
+        primes = new boolean[range +1];
+    }
 
     public long[] getPrimes(int n) {
 
@@ -16,22 +21,25 @@ public class EratosthenesSieve implements PrimesAlgorithm {
             factors[i + 1] = getNextPrime(factors[i]);
         }
         return factors;
-
-
     }
 
     private long getNextPrime(long lastPrime){
-
         if(lastPrime * 2 > range){
             range = range*2;
+            primes = new boolean[range +1];
+            return calculateNextPrime(lastPrime);
         }
-        boolean[] primes = new boolean[range +1];
+        return calculateNextPrime(lastPrime);
+    }
+
+    private long calculateNextPrime(long lastPrime){
+
         for(int i = 0; i < primes.length; i++){
             primes[i] = true;
         }
         primes[0] = false;
         primes[1] = false;
-        for(int i = 2; i <= range; i++){
+        for(int i = 2; i <= Math.sqrt(range); i++){
             if(primes[i]){
                 for(int j = 2; i*j <= range; j++){
                     primes[i*j] = false;
